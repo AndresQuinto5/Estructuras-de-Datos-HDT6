c9d957e3a35be1aff94e535b250f4fc13d21234c
lquezada <que18028@uvg.edu.gt>
2019-03-03 09:55:40 -0600

    Mapfactory Semi-complete

--- MapFactory.java
+++ MapFactory.java
@@ -3,4 +3,48 @@
 // Autores: Luis Quezada 18028 & Andrea Paniagua 18733
 // Fecha: 8 de marzo 2019
 
+/**
+* <h1>Map Factory</h1>
+* Implements the Factory design pattern.
+* <p>
+* @author  Luis Quezada 18028 & Andrea Paniagua 18733
+* @version 1.0
+* @since   2019-03-8
+*/
 
+import java.util.*;
+
+class MapFactory<K,V> {
+	
+	//selecciona la implementacion a utilizar para un map
+	// Pueden ser: HashMap, TreeMap y LinkedHashMap
+	
+	/**
+	*
+	* pre: receives a number to indicate the type of map to use
+	* post: returns the selected map
+	* @param type Type of map to use
+	* @return Map<E> Selected map
+	*/
+	
+	static int HASHMAP = 1;
+	static int TREEMAP = 2;
+	static int LINKED = 3;
+	
+	public MapFactory() {}
+   
+	public Map<K,V> getMap(int type) {
+    // seleccion de la implementacion a utilizar:
+    
+		if (type == HASHMAP) {
+			return new HashMap<K,V>(); //regresa HashMap
+		
+		} if (type == TREEMAP) {
+			return new TreeMap<K,V>(); //regresa TreeMap
+		
+		} else {
+		
+			return new LinkedHashMap<K,V>(); // regresa LinkedHashMap
+		}
+	}
+}
